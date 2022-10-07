881fd41c4a58fd34359e08b2fb6c02c7f82ace88
kiespetch1 <kiespetch@ya.ru>
2022-10-07 09:25:38 +0300

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BitSlider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChooseFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ChooseFile
+            // 
+            this.ChooseFile.Location = new System.Drawing.Point(100, 12);
+            this.ChooseFile.Name = "ChooseFile";
+            this.ChooseFile.Size = new System.Drawing.Size(111, 34);
+            this.ChooseFile.TabIndex = 0;
+            this.ChooseFile.Text = "Выбрать файл";
+            this.ChooseFile.UseVisualStyleBackColor = true;
+            this.ChooseFile.Click += new System.EventHandler(this.ChooseFile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ChooseFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 178);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ChooseFile);
+            this.Name = "Form1";
+            this.Text = "Побитовая конвертация";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Button ChooseFile;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BitSlider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        private void ChooseFile_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = @"c:\";
+                openFileDialog.Filter = @"txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+
+                    var a = (int) new FileInfo(filePath).Length;
+                    var name = Path.GetFileName(filePath);
+                    var workingPath = Path.GetDirectoryName(filePath);
+                    var filePath2 = $"{workingPath}\\converted_{name}";
+                    var resultName = Path.GetFileName(filePath2);
+                    var array = new byte[a];
+                    using (FileStream fsSource = new FileStream(filePath,
+                               FileMode.Open, FileAccess.Read))
+                    {
+                        int b = fsSource.Read(array, 0, a);
+                    }
+
+                    using (var fstream = new FileStream(filePath2, FileMode.Create))
+                    {
+                        foreach (byte ab in array)
+                        {
+                            fstream.WriteByte((byte) (ab + 1));
+                        }
+                    }
+                    Label label1 = new Label();
+                    label1.Location = new Point(44, 63);
+                    label1.Name = "label1";
+                    label1.Size = new Size(224, 40);
+                    label1.TabIndex = 1;
+                    label1.Text = $"Выбранный файл: {name}";
+                    label1.TextAlign = ContentAlignment.MiddleCenter;
+                    Controls.Add(label1);
+                    
+                    Label label2 = new Label();
+                    label2.Location = new Point(44, 112);
+                    label2.Name = "label2";
+                    label2.Size = new Size(224, 40);
+                    label2.TabIndex = 2;
+                    label2.Text = $"Конвертированный файл: {resultName}";
+                    label2.TextAlign = ContentAlignment.MiddleCenter;
+                    Controls.Add(label2);
+                    
+                }
+            }
+        }
+
+        
+    }
+}
